b9144cb666d19827f0ba3860fa7c267d8b525f22
Michael Ly <michael@bitzesty.com>
2013-03-13 16:14:29 +0000

    Styled the various simple pages

---
+++ wp-content/themes/nousdesign/page-contact.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template Name: Contact
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.1
+ */
+
+get_header(); ?>
+  <div class="contact-page-height">
+    <div class="row-fluid page-contact centered">
+      <div class="span12 content-text">
+        <div class="contact-background">
+          <em>Nous</em>
+          <span>Design</span>
+        </div>
+        <div class="contact-foreground">
+          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+            <h1>
+              <em>Nous</em>Design
+            </h1>
+            <?php the_content(); ?>
+
+          <?php endwhile; else: ?>
+            <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+          <?php endif; ?>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<?php get_footer(); ?>
--- wp-content/themes/nousdesign/page-services.php
+++ wp-content/themes/nousdesign/page-services.php
@@ -1,7 +1,16 @@
-<?php get_header(); ?>
+<?php
+/**
+ * Template Name: Services
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.1
+ */
 
-  <div class="row-fluid">
-    <div class="span12">
+get_header(); ?>
+
+  <div class="row-fluid page-services">
+    <div class="span12 content-text">
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
         <?php the_content(); ?>
